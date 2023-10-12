5acd82714a685c6faf4e51e1b58a592d287c468d
Яна <106766066+cherrryana@users.noreply.github.com>
2023-10-12 13:01:03 +0200

    :)

---
+++ lab3/Array3d.cs
@@ -0,0 +1,206 @@
+﻿namespace oop_lab3;
+
+public class Array3d <T>
+{
+    public int Dim0 { get; }
+    public int Dim1 { get; }
+    public int Dim2 { get; }
+    
+    private T[] array3d;
+
+    public Array3d(int dim0, int dim1, int dim2)
+    {
+        Dim0 = dim0;
+        Dim1 = dim1;
+        Dim2 = dim2;
+
+        array3d = new T[dim0 * dim1 * dim2];
+    }
+
+    public T this[int i, int j, int k]
+    {
+        get
+        {
+            return (checkIndex(i, j, Dim0, Dim1) && checkIndex(k, Dim2)) ?      // в индексаторе не всегда учитывается провекра на неотрицательность индекса и на то,
+                                                                                // что он меньше своего Dim, поэтому дальше везде стоят доп. проверки
+                array3d[k * Dim0 * Dim1 + j * Dim0 + i] : throw new Exception("Index is out of range");
+        }
+        set
+        {
+            array3d[k * Dim0 * Dim1 + j * Dim0 + i] = (checkIndex(i, j, Dim0, Dim1) && checkIndex(k, Dim2)) ? 
+                value : throw new Exception("Index is out of range");
+        }
+    }
+
+    public void SetValues0(int i, T[][] matrix)
+    {
+        if (checkIndex(i, Dim0))
+        {
+            for (int j = 0; j < matrix.Length; j++)
+            {
+                for (int k = 0; k < matrix[j].Length; k++)
+                    this[i, j, k] = matrix[j][k];
+            }
+        }
+    }
+    
+    public void SetValues1(int j, T[][] matrix)
+    {
+        if (checkIndex(j, Dim1))
+        {
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int k = 0; k < matrix[i].Length; k++)
+                    this[i, j, k] = matrix[i][k];
+            }
+        }
+    }
+    
+    public void SetValues2(int k, T[][] matrix)
+    {
+        if (checkIndex(k, Dim2))
+        {
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                    this[i, j, k] = matrix[i][j];
+            }
+        }
+    }
+
+    public void SetValues01(int i, int j, T[] array)
+    {
+        if (checkIndex(i, j, Dim0, Dim1))
+        {
+            for (int k = 0; k < array.Length; k++)
+                this[i, j, k] = array[k];
+        }
+    }
+    
+    public void SetValues02(int i, int k, T[] array)
+    {
+        if (checkIndex(i, k, Dim0, Dim2))
+        {
+            for (int j = 0; j < array.Length; j++)
+                this[i, j, k] = array[j];
+        }
+    }
+    
+    public void SetValues12(int j, int k, T[] array)
+    {
+        if (checkIndex(j, k, Dim1, Dim2))
+        {
+            for (int i = 0; i < array.Length; i++)
+                this[i, j, k] = array[i];
+        }
+    }
+
+    public T[,] GetValues0(int i)
+    {
+        T[,] result = new T[Dim1, Dim2];
+
+        if (checkIndex(i, Dim0))
+        {
+            for (int j = 0; j < Dim1; j++)
+            {
+                for (int k = 0; k < Dim2; k++)
+                    result[j, k] = this[i, j, k];
+            }
+        }
+
+        return result;
+    }
+    
+    public T[,] GetValues1(int j)
+    {
+        T[,] result = new T[Dim0, Dim2];
+
+        if (checkIndex(j, Dim1))
+        {
+            for (int i = 0; i < Dim0; i++)
+            {
+                for (int k = 0; k < Dim2; k++)
+                    result[i, k] = this[i, j, k];
+            }
+        }
+
+        return result;
+    }
+    
+    public T[,] GetValues2(int k)
+    {
+        T[,] result = new T[Dim0, Dim1];
+        
+        if (checkIndex(k, Dim2))
+        {
+            for (int i = 0; i < Dim0; i++)
+            {
+                for (int j = 0; j < Dim1; j++)
+                    result[i, j] = this[i, j, k];
+            }
+        }
+
+        return result;
+    }
+    
+    public T[] GetValues01(int i, int j)
+    {
+        T[] result = new T[Dim2];
+
+        if (checkIndex(i, j, Dim0, Dim1))
+        {
+            for (int k = 0; k < Dim2; k++)
+                result[k] = this[i, j, k];
+        }
+
+        return result;
+    }
+    
+    public T[] GetValues02(int i, int k)
+    {
+        T[] result = new T[Dim1];
+
+        if (checkIndex(i, k, Dim0, Dim2))
+        {
+            for (int j = 0; j < Dim1; j++)
+                result[j] = this[i, j, k];
+        }
+
+        return result;
+    }
+    
+    public T[] GetValues12(int j, int k)
+    {
+        T[] result = new T[Dim0];
+
+        if (checkIndex(j, k, Dim1, Dim2))
+        {
+            for (int i = 0; i < Dim0; i++)
+                result[i] = this[i, j, k];
+        }
+
+        return result;
+    }
+
+    public void npfFill(T value)        // я оставила только метод npFill, т.к. в силу произвольности типа данных не думаю, что есть смысл
+                                        // делать отдельные функции для заполнения массива определенным типом данных - интовым (нулями и единицами),
+                                        // удобнее будет написать npFill(0) или npFill(1)
+    {
+        for (int i = 0; i < array3d.Length; i++)
+            array3d[i] = value;
+    }
+
+    private bool checkIndex(int i, int dim)
+    {
+        if (i >= 0 && i < dim)
+            return true;
+        throw new Exception("Index is out of range");
+    }
+
+    private bool checkIndex(int i, int j, int dim0, int dim1)
+    {
+        if (i < dim0 && j < dim1 && i >= 0 && j >= 0)
+            return true;
+        throw new Exception("Index is out of range");
+    }
+}
---
+++ lab3/Program.cs
@@ -0,0 +1,29 @@
+﻿using oop_lab3;
+
+class Program
+{
+    public static void Main(String[] str)
+    {
+        Array3d<int> array3d = new Array3d<int>(2, 3, 4);
+        int[][] matrix = new int[][] { new int[] { 1, 2, 3, 4}, 
+            new int[] { 5, 6, 7 }, 
+            new int[] { 8, 9 } };
+        int[] array = new int[] { 11, 12 };
+        
+        array3d.SetValues0(0, matrix);
+        array3d.SetValues01(1, 2, array);
+        array3d[0, 0, 0] = -11;
+        
+        int[,] result1 = array3d.GetValues0(0);
+        for (int j = 0; j < array3d.Dim1; j++)
+        {
+            for (int k = 0; k < array3d.Dim2; k++)
+                Console.WriteLine(result1[j, k]);
+        }
+
+        Console.WriteLine("\n");
+        int[] result2 = array3d.GetValues01(1, 2);
+        for (int i = 0; i < result2.Length; i++)
+            Console.WriteLine(result2[i]);
+    }
+}
